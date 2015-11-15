419f4a45c12ed1ba6d4d50c696ddf406ad0ed07a
drafteee <sergej_-96@mail.ru>
2015-11-15 23:38:54 +0300

    input

---
+++ MetrologyTwo/MetrologyTwo/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace MetrologyTwo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.TextBoxOfMainText = new System.Windows.Forms.TextBox();
+            this.TextBoxOfResultText = new System.Windows.Forms.TextBox();
+            this.ButtonOfProccedTask = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(674, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+           // this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // TextBoxOfMainText
+            // 
+            this.TextBoxOfMainText.Location = new System.Drawing.Point(0, 27);
+            this.TextBoxOfMainText.Multiline = true;
+            this.TextBoxOfMainText.Name = "TextBoxOfMainText";
+            this.TextBoxOfMainText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBoxOfMainText.Size = new System.Drawing.Size(408, 318);
+            this.TextBoxOfMainText.TabIndex = 1;
+            // 
+            // TextBoxOfResultText
+            // 
+            this.TextBoxOfResultText.Enabled = false;
+            this.TextBoxOfResultText.Location = new System.Drawing.Point(414, 25);
+            this.TextBoxOfResultText.Multiline = true;
+            this.TextBoxOfResultText.Name = "TextBoxOfResultText";
+            this.TextBoxOfResultText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBoxOfResultText.Size = new System.Drawing.Size(257, 276);
+            this.TextBoxOfResultText.TabIndex = 2;
+            // 
+            // ButtonOfProccedTask
+            // 
+            this.ButtonOfProccedTask.Enabled = false;
+            this.ButtonOfProccedTask.Location = new System.Drawing.Point(415, 306);
+            this.ButtonOfProccedTask.Name = "ButtonOfProccedTask";
+            this.ButtonOfProccedTask.Size = new System.Drawing.Size(255, 38);
+            this.ButtonOfProccedTask.TabIndex = 3;
+            this.ButtonOfProccedTask.Text = "Procced Task";
+            this.ButtonOfProccedTask.UseVisualStyleBackColor = true;
+            this.ButtonOfProccedTask.Click += new System.EventHandler(this.ButtonOfProccedTask_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 346);
+            this.Controls.Add(this.ButtonOfProccedTask);
+            this.Controls.Add(this.TextBoxOfResultText);
+            this.Controls.Add(this.TextBoxOfMainText);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Kozlovski Sergei Metrology Spen";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox TextBoxOfMainText;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.TextBox TextBoxOfResultText;
+        private System.Windows.Forms.Button ButtonOfProccedTask;
+    }
+}
+
---
+++ MetrologyTwo/MetrologyTwo/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetrologyTwo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public struct InfoAboutVariables
+        {
+            public string TitleOfVariable;
+            public int AmountVariableInCode;
+            public InfoAboutVariables(string StringOfName, int AmountOfMeetVariable)
+            {
+                TitleOfVariable = StringOfName;
+                AmountVariableInCode = AmountOfMeetVariable;
+            }
+        }
+
+      
+
+        void DeleteCommentsInCode(string NewDataOfMainFileWithoutComments)
+        {
+            bool FlagOfStringComment = false;
+
+            for (int i = 0; i < TextBoxOfMainText.Text.Length; i++)
+            {
+                if ((TextBoxOfMainText.Text[i] == '/') && (TextBoxOfMainText.Text[i + 1] == '/'))
+                    while (TextBoxOfMainText.Text[i] != '\r')
+                    {
+                        i++;
+                    }
+
+                if ((TextBoxOfMainText.Text[i] == '/') && (TextBoxOfMainText.Text[i + 1] == '*'))
+                {
+                    while (FlagOfStringComment == false)
+                    {
+                        if ((TextBoxOfMainText.Text[i] == '*') && (TextBoxOfMainText.Text[i + 1] == '/'))
+                            FlagOfStringComment = true;
+                        i++;
+                    }
+                    i += 2;
+                    FlagOfStringComment = false;
+                }
+                if (i >= TextBoxOfMainText.Text.Length)
+                    NewDataOfMainFileWithoutComments += "";
+                else
+                    NewDataOfMainFileWithoutComments += Convert.ToString(TextBoxOfMainText.Text[i]);
+            }
+            TextBoxOfMainText.Text = NewDataOfMainFileWithoutComments;
+        }
+
+        List<InfoAboutVariables> DoFindVariablesInLine(ref List<InfoAboutVariables> NamesOfVariablesOurCode, string[] ContainsOfLineWithCode)
+        {
+            for(int i = 0; i< ContainsOfLineWithCode.Length;i++)
+            {
+                if((ContainsOfLineWithCode[i].Contains("=") == true) || (ContainsOfLineWithCode[i].Contains(";") == true))
+                {
+                    if (ContainsOfLineWithCode[i].Contains(";") == true)
+                    {
+
+                        ContainsOfLineWithCode[i] = ContainsOfLineWithCode[i].Replace(";", "");
+                        NamesOfVariablesOurCode.Add(new InfoAboutVariables(ContainsOfLineWithCode[i], 0));
+                    }
+                    else
+                    {
+                        NamesOfVariablesOurCode.Add(new InfoAboutVariables(ContainsOfLineWithCode[i - 1], 0));
+                        break;
+                    }
+                }
+            }
+            return NamesOfVariablesOurCode;
+        }
+        List<InfoAboutVariables> DoFindAmountVariables(ref List<InfoAboutVariables> OurVariablesInCode, string OurAnalyzingCode)
+        {
+            string[] ContainsOfLineWithCode = {};
+            string StringOfVAR = "var", StringOfCONST = "const", LineWithCode = "";
+         
+            for (int i = 0; i < TextBoxOfMainText.Lines.Length; i++)
+            {
+                LineWithCode = TextBoxOfMainText.Lines[i];
+               
+                if ((LineWithCode.Contains(StringOfVAR) == true)|| (LineWithCode.Contains(StringOfCONST) == true))
+                {
+                   do
+                   {
+                        LineWithCode = TextBoxOfMainText.Lines[i];
+                        ContainsOfLineWithCode = LineWithCode.Split(' ');
+                        OurVariablesInCode = DoFindVariablesInLine(ref OurVariablesInCode, ContainsOfLineWithCode);
+                        i++;
+                        
+                   } while (LineWithCode.Contains(';') != true) ;
+
+                   i--;   
+                }
+            }
+            return OurVariablesInCode;
+        }
+
+        List<InfoAboutVariables> DoFindAmountOfMeetOfVariables(ref List<InfoAboutVariables> AmountMeetOfVariables, string OurAnalyzingCode)
+        {
+            string LineWithCode = "";
+            InfoAboutVariables[] MyArrayOfCopyMyList = AmountMeetOfVariables.ToArray();
+
+            for (int i = 0; i < TextBoxOfMainText.Lines.Length; i++)
+            {
+                LineWithCode = TextBoxOfMainText.Lines[i];
+
+                for (int j = 0;j < AmountMeetOfVariables.Count; j++)
+                {
+                    if (LineWithCode.Contains(AmountMeetOfVariables[j].TitleOfVariable) == true)
+                        MyArrayOfCopyMyList[j].AmountVariableInCode++;
+                }
+            }
+      
+            AmountMeetOfVariables = MyArrayOfCopyMyList.ToList<InfoAboutVariables>();
+
+            return AmountMeetOfVariables.Distinct().ToList();
+        }
+
+        void DoOutputInfomationAboutResult(List<InfoAboutVariables>  ResultVariables)
+        {
+            for(int i = 0;i < ResultVariables.Count;i++)
+            {
+                TextBoxOfResultText.Text += ResultVariables[i].TitleOfVariable + "-" + Convert.ToString(ResultVariables[i].AmountVariableInCode - 1) + Environment.NewLine; 
+            }
+        }
+        void DoMainTask()
+        {
+            string NewDataOfMainFileWithoutComments = "";
+         
+            List<InfoAboutVariables> ResultVariables = new List<InfoAboutVariables>();
+
+            DeleteCommentsInCode(NewDataOfMainFileWithoutComments);
+
+            ResultVariables = DoFindAmountVariables(ref ResultVariables, NewDataOfMainFileWithoutComments);
+
+            ResultVariables = DoFindAmountOfMeetOfVariables(ref ResultVariables, NewDataOfMainFileWithoutComments);
+
+            DoOutputInfomationAboutResult(ResultVariables);
+        }
+        private void ButtonOfProccedTask_Click(object sender, EventArgs e)
+        {
+            DoMainTask();
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName, Encoding.Default);
+
+                TextBoxOfMainText.Text = sr.ReadToEnd();
+                sr.Close();
+
+                if (TextBoxOfMainText.Text != "")
+                    ButtonOfProccedTask.Enabled = true;
+            }
+        }
+    }
+}
